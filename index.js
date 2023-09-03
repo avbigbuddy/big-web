11c393cc5b6caffad90855b5368c544337cd3ffd
avbigbuddy <136998037+avbigbuddy@users.noreply.github.com>
2023-09-03 21:48:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const nodemailer = require('nodemailer')
-const cors = require("cors")
+//const cors = require("cors")
 const app = express()
 app.use(express.json());
-app.use(cors())
+//app.use(cors())
 
 app.use(express.static(__dirname));
 app.get("/", (req, res) => {
